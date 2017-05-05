9b4332baec95752adb9bf6b4fa08bd8ebc940bcc
Jason Hanna <jason.hanna01@gmail.com>
2017-05-05 15:19:05 -0600

    refactor geocodeAddress method

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const yargs = require('yargs');
 const geocode = require('./geocode/geocode')
 
 const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  }
+})
+.help()
+.alias('help', 'h')
+.argv;
 
-geocode.geocodeAddress(argv.address);
-
-var encodedAddress = encodeURIComponent(argv.address);
+geocode.geocodeAddress(argv.address, callback = (error, result) => {
+  if (error){
+    console.log(error);
+  } else {
+    console.log(JSON.stringify(result, undefined, 2));
+  }
+});
