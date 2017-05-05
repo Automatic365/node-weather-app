6b1865637c3ae6be1dce33a17754a08c84c8d494
Jason Hanna <jason.hanna01@gmail.com>
2017-05-05 17:16:15 -0600

    add darksky api to app.js

--- app.js
+++ app.js
@@ -14,10 +14,23 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
-geocode.geocodeAddress(argv.address, callback = (error, result) => {
-  if (error){
-    console.log(error);
+// geocode.geocodeAddress(argv.address, callback = (error, result) => {
+//   if (error){
+//     console.log(error);
+//   } else {
+//     console.log(JSON.stringify(result, undefined, 2));
+//   }
+// });
+
+const request = require('request');
+
+request({
+  url: 'https://api.forecast.io/forecast/4a04d1c42fd9d32c97a2c291a32d5e2d/39.9396284,-75.18663959999999',
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(result, undefined, 2));
+    console.log('Unable to fetch weather.');
   }
 });
